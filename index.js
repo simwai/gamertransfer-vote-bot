fe54ef05025e20b1474f6b91e8edd761af8c07f5
simwai <SimonWaiblinger@live.at>
2021-05-18 00:35:54 +0200

    changed schedule package

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-const { chromium } = require('playwright')
-const schedule = require('node-schedule')
-const randomUseragent = require('random-useragent')
+const Cabin = require('cabin')
+const Bree = require('bree')
+const Graceful = require('@ladjs/graceful')
 
-const config = require('./config')
-const useragent = randomUseragent.getRandom()
-
-// run the bot every 4 hours
-schedule.scheduleJob('0 0 0/4 1/1 * ? *', async () => {
-  console.log('Job runs')
-  await run()
+const bree = new Bree({
+  logger: new Cabin(),
+  jobs: [
+    {
+      name: 'bot',
+      interval: '4h'
+    }
+  ]
 })
 
-async function run () {
-  const browser = await chromium.launch({ headless: true, userAgent: useragent })
-  const context = await browser.newContext()
-  const page = await context.newPage()
-  await page.goto('https://gamertransfer.com/dashboard.html?m=17')
+// handle graceful reloads, pm2 support, and events like SIGHUP, SIGINT, etc.
+const graceful = new Graceful({ brees: [bree] })
+graceful.listen()
 
-  await page.waitForTimeout(3000)
-
-  await page.click('text=einloggen')
-  await page.waitForTimeout(3000)
-
-  await page.fill(':nth-match(input, 1)', config.username)
-
-  await page.waitForTimeout(3000)
-  await page.fill(':nth-match(input, 2)', config.password)
-
-  await page.waitForTimeout(3000)
-  await page.click('text=Log in')
-
-  await page.waitForTimeout(1500)
-  const loginFailed = await page.$('text=Fehler: E-Mail oder Passwort falsch')
-
-  if (loginFailed) {
-    console.log('login failed')
-    return
-  }
-
-  await page.waitForTimeout(3000)
-  const pushButtons = await page.$$('.push')
-
-  for (const el of pushButtons) {
-    await el.click()
-    await page.waitForTimeout(1500)
-  }
-}
+// start all jobs (this is the equivalent of reloading a crontab):
+bree.start()
